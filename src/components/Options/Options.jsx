0ce52ce643053a57b614e83467aa586ac292b12c
NovichokUA <zubr7333@gmail.com>
2024-02-16 21:53:23 +0200

    add css btn

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,19 +1,29 @@
 import css from "./Options.module.css";
+import {
+  FaHeart,
+  FaRegFaceMeh,
+  FaHeartCrack,
+  FaRegTrashCan,
+} from "react-icons/fa6";
 
 export const Options = ({ onUpdate, totalFeedback, reset }) => {
   return (
     <div className={css.container}>
       <button className={css.btn} onClick={() => onUpdate("good")}>
+        <FaHeart className={css.myIconGood} size="15" />
         Good
       </button>
       <button className={css.btn} onClick={() => onUpdate("neutral")}>
+        <FaRegFaceMeh className={css.myIconGood} size="15" />
         Neutral
       </button>
       <button className={css.btn} onClick={() => onUpdate("bad")}>
+        <FaHeartCrack className={css.myIconGood} size="14" />
         Bad
       </button>
       {totalFeedback > 0 && (
         <button className={css.btn} onClick={reset}>
+          <FaRegTrashCan size="14" />
           Reset
         </button>
       )}
